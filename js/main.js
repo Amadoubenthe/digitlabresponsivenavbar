0b50c23b54b21ba23e4c7bf7d737b03ce1a33fcf
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-07-28 12:27:11 +0000

    Add Project

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Recuper le boutton qui a l'id 'btnToggler'
+
+// Recuper l'élément HTML avec la classe "navigation"
+
+// Ajouter un écouteur d'événement "click" sur l'élément btnToggler, et ajouter ou retirer la classe "active" sur l'élément navigation
